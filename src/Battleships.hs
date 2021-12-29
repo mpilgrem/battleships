f9cd4f95521138a9a8467f2fe8ca525c6b84e623
Mike Pilgrem <mpilgrem@users.noreply.github.com>
2021-12-29 00:39:57 +0000

    Refactor library code

--- src/Battleships.hs
+++ src/Battleships.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE BangPatterns  #-}
 {-# LANGUAGE TupleSections #-}
 
 module Battleships where
@@ -34,6 +35,28 @@ data Board = Board
 ortho :: Orient -> Orient
 ortho H = V
 ortho V = H
+
+index :: Orient -> Position -> Int
+index H (i, _) = i
+index V (_, j) = j
+
+origin :: Orient -> Position -> Position
+origin H (_, j) = (0, j)
+origin V (i, _) = (i, 0)
+
+location :: Orient -> Int -> Int -> Position
+location H j i = (i, j)
+location V i j = (i, j)
+
+dim :: Orient -> Board -> Int
+dim H b = width b
+dim V b = height b
+
+count :: (a -> Bool) -> [a] -> Int
+count p = go 0
+ where go !n [] = n
+       go !n (x:xs) | p x       = go (n + 1) xs
+                    | otherwise = go n xs
 
 emptyBoard :: Int -> Int -> Board
 emptyBoard w h = Board
@@ -90,59 +113,31 @@ mkInfo :: Vector Int -> Vector Int -> Array Orient (Vector Int)
 mkInfo hInfo vInfo = array (H, V) [ (H, hInfo), (V, vInfo) ]
 
 valid :: Orient -> Board -> Int -> Position -> Bool
-valid o b s (i, j)
-  | o == H
-  , i + s > width b = False
-  | o == H
-  , s > info b A.! H V.! j = False
-  | o == V
-  , j + s > height b = False
-  | o == V
-  , s > info b A.! V V.! i = False
-  | countBoat' b (o, (i, j)) s + s > limit b (o, (i, j)) = False
-  | otherwise = isRun o b (i, j) s
+valid o b s p
+  | index o p + s > dim o b = False
+  | s > limit' b o (index (ortho o) p) = False
+  | countBoat' b (o, p) s + s > limit b (o, p) = False
+  | otherwise = isRun o b p s
 
 limit :: Board -> RunOrigin -> Int
-limit b (o, (i, j)) = info b A.! o V.! rc
- where
-  rc = case o of
-         H -> j
-         V -> i
+limit b (o, p) = limit' b o (index (ortho o) p)
 
-countBoat :: Orient -> Board -> Int -> Int
-countBoat o b i = let count n j = if cells b A.! p == Just Boat
-                                    then n + 1
-                                    else n
-                       where
-                        p = case o of
-                              H -> (j, i)
-                              V -> (i, j)
-                  in  foldl' count 0 [0 .. l - 1]
+limit' :: Board -> Orient -> Int -> Int
+limit' b o i = info b A.! o V.! i
 
+countBoat :: Board -> Orient -> Int -> Int
+countBoat b o i = count test [0 .. dim o b - 1]
  where
-  l = case o of
-        H -> width b
-        V -> height b
+  test j = cells b A.! (location o i j) == Just Boat
 
 countBoat' :: Board -> RunOrigin -> Int -> Int
-countBoat' b (o, p@(i, j)) s = let count n p' = if cells b A.! p' == Just Boat
-                                                  then n + 1
-                                                  else n
-                               in  foldl' count 0 (pre <> post)
+countBoat' b (o, p) s = count test (pre <> post)
  where
-  origin = case o of
-             H -> (0, j)
-             V -> (i, 0)
-  preLen = case o of
-             H -> i
-             V -> j
-  pre = run o origin preLen
-  postLen = case o of
-              H -> width b - i - s
-              V -> height b  - j - s
+  test p' = cells b A.! p' == Just Boat
+  pre = run o (origin o p) (index o p)
+  postLen = dim o b - index o p - s
   post = run o (p `add` scale s f) postLen
   (_, _, _, f) = dirs o
-
 
 isNotBoat :: Board -> Position -> Bool
 isNotBoat b (i, j)
@@ -165,14 +160,11 @@ isRun o b p s = any (isBlank b) run' &&
 isValidRun :: Orient -> Board -> [Position] -> Bool
 isValidRun o b = all validCell
  where
-  validCell p@(i, j) = case cells b A.! p of
+  validCell p = case cells b A.! p of
     Just Sea  -> False
     Just Boat -> True
-    Nothing   -> countBoat o b rc < info b A.! o V.! rc
-   where
-    rc = case o of
-           H -> j
-           V -> i
+    Nothing   -> let idx = index (ortho o) p
+                 in  countBoat b o idx < limit' b o idx
 
 run :: Orient -> Position -> Int -> [Position]
 run o p s = map (\m -> p `add` scale m f) [0 .. s - 1]
@@ -180,9 +172,9 @@ run o p s = map (\m -> p `add` scale m f) [0 .. s - 1]
   (_, _, _, f) = dirs o
 
 validNeighbours :: Board -> Orient -> Position -> Int -> [Position]
-validNeighbours b o (i, j) s = filter onBoard (neighbours o (i, j) s)
+validNeighbours b o p s = filter onBoard (neighbours o p s)
  where
-  onBoard (i', j') = i' >= 0 && j' >= 0 && i' < width b && j' < height b
+  onBoard (i, j) = i >= 0 && j >= 0 && i < width b && j < height b
 
 neighbours :: Orient -> Position -> Int -> [Position]
 neighbours o p s = (p `add` b) : (p `add` scale s f) : foldl' lrs [] [-1, 0 .. s]
@@ -203,43 +195,31 @@ add (x, y) (dx, dy) = (x + dx, y + dy)
 scale :: Int -> Direction -> Direction
 scale m (dx, dy) = (m * dx, m * dy)
 
-runs :: Orient -> Board -> Int -> [Position]
-runs o b s = filter (valid o b s) (A.indices $ cells b)
-
 allRuns :: Board -> Int -> [RunOrigin]
-allRuns b s = map (H,) (runs H b s) <> map (V,) (runs V b s)
+allRuns b s = runs H <> runs V
+ where
+  runs o = map (o,) $ filter (valid o b s) (A.indices $ cells b)
 
 isComplete :: Board -> Bool
-isComplete b = all (isComplete' H b) [0 .. height b - 1] &&
-               all (isComplete' V b) [0 .. width b - 1]
-
-isComplete' :: Orient -> Board -> Int -> Bool
-isComplete' o b i = countBoat o b i == info b A.! o V.! i
+isComplete b = all (isComplete' H) [0 .. height b - 1] &&
+               all (isComplete' V) [0 .. width b - 1]
+ where
+  isComplete' o i = countBoat b o i == limit' b o i
 
 solve :: Board -> Fleet -> Maybe Board
-solve b f
-  | null f = if isComplete b
-               then Just b
-               else Nothing
-  | otherwise = let s = head f
-                    rs = allRuns b s
-                in  if null rs
-                      then Nothing
-                      else let bs = map (\r -> place b r s) rs
-                           in  solve' bs (tail f)
+solve b [] = if isComplete b then Just b else Nothing
+solve b (s:ss) = let rs = allRuns b s
+                     bs = map (place b s) rs
+                 in  if null rs then Nothing else solve' bs ss
 
 solve' :: [Board] -> Fleet -> Maybe Board
 solve' [] _ = error "No boards!"
 solve' [b] f = solve b f
-solve' (b:bs) f =
-  case solve b f of
-    Nothing -> solve' bs f
-    Just b' -> Just b'
+solve' (b:bs) f = maybe (solve' bs f) Just (solve b f)
 
-place :: Board -> RunOrigin -> Int -> Board
-place b (o, p) s =
-  let cs = cells b
-      cs' = cs A.// (boats <> seas)
+place :: Board -> Int -> RunOrigin -> Board
+place b s (o, p) =
+  let cs = cells b A.// (boats <> seas)
       boats = map (,Just Boat) (run o p s)
       seas = map (,Just Sea) (validNeighbours b o p s)
-  in  b {cells = cs'}
+  in  b {cells = cs}
